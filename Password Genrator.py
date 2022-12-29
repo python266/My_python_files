4146f57d246669f76de88283fc082c7f475f6ded
Himesh <121535663+python266@users.noreply.github.com>
2022-12-29 13:08:17 +0530

    Add files via upload

---
+++ Password Genrator.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+s1= string.ascii_lowercase
+s2= string.ascii_uppercase
+s3= string.digits
+s4= string.punctuation
+
+user = int(input("Enter your length of your password: "))
+
+s = []
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+print("".join(random.sample(s, user)))
